aea983acd55d8d4ca9179c3e6bdc63ee85b922d7
tygris <youssif6920@gmail.com>
2025-08-21 22:11:46 -0600

    analog sticks

--- src/input.rs
+++ src/input.rs
@@ -19,9 +19,13 @@ pub fn new() -> Self{
      let mut device = uinput::default()
         //this is called if device init is attempted but uinput module is not loaded by the system
         .expect("\x1b[31m FAILED TO CREATE DEVICE? MAKE SURE UINPUT IN LOADED!!!!
-            \x1b[0m ").name("Joyer Controller")
+            \x1b[0m ").name("PlayPortDevice")
         .expect("failed").event(uinput::event::Controller::All).expect(
             "faield")
+        .event(Absolute::Position(Position::X)).expect("ABS X")
+        .event(Absolute::Position(Position::Y)).expect("ABS Y")
+        .event(Absolute::Position(Position::RX)).expect("ABS RX")
+        .event(Absolute::Position(Position::RY)).expect("ABS RY")
         .create().expect("");
     
     return JoyDevice { device: device }
@@ -35,17 +39,24 @@ pub fn handle_input(&mut self, code: String){
     dbg!(key);
     match key{
         "A" => { self.device.press(&Controller::GamePad(GamePad::South)).unwrap();
+            self.device.synchronize().unwrap();
+            print!("\x1b[21m HEHEHEHEH \x1b[0m");
             },
         "B" => { self.device.press(&Controller::GamePad(GamePad::East)).unwrap();
+            self.device.synchronize().unwrap();
             },
         "Y" => { self.device.press(&Controller::GamePad(GamePad::North)).unwrap();
+            self.device.synchronize().unwrap();
             },
         "X" => { self.device.press(&Controller::GamePad(GamePad::West)).unwrap();
+            self.device.synchronize().unwrap();
             },
         "LB" => { self.device.press(&Controller::GamePad(GamePad::TL)).unwrap();
-                }
+                },
+        "LB1" => { self.device.press(&Controller::GamePad(GamePad::TL2)).unwrap();}
         "RB" => { self.device.press(&Controller::GamePad(GamePad::TR)).unwrap();
-                }
+                },
+        "RB1" => { self.device.press(&Controller::GamePad(GamePad::TR2)).unwrap();}
         //DPAD STUFF
         "DPAD_DOWN" => {self.device.press(&Controller::DPad(controller::DPad::Down)).unwrap();
                 
@@ -76,9 +87,11 @@ pub fn handle_input(&mut self, code: String){
         "X" => { self.device.release(&Controller::GamePad(GamePad::West)).unwrap();
             },
         "LB" => { self.device.release(&Controller::GamePad(GamePad::TL)).unwrap();
-                }
+                },
+        "LB1" => { self.device.release(&Controller::GamePad(GamePad::TL2)).unwrap();}
         "RB" => { self.device.release(&Controller::GamePad(GamePad::TR)).unwrap();
-                }
+                },
+        "RB1" => { self.device.release(&Controller::GamePad(GamePad::TR2)).unwrap();}
         //dpad release 
         "DPAD_DOWN" => {self.device.release(&Controller::DPad(controller::DPad::Down)).unwrap();
                 
@@ -96,6 +109,34 @@ pub fn handle_input(&mut self, code: String){
         _ => { println!("UNKNOWN INPUT!");}
     }
     self.device.synchronize().unwrap();
+  }
+  if let Some(ax) = code.strip_prefix("LS:"){
+      if let Some((axis, v)) = ax.split_once(":"){
+          if let Ok(mut v) = v.parse::<i32>(){
+              v = v.clamp(-32768, 32767);
+              match axis {
+                  "X" => { let _ = self.device.position(&Position::X, v);},
+                  "Y" => { let _ = self.device.position(&Position::Y, v);},
+                  _ => {}
+              }
+              let _ = self.device.synchronize();
+          }
+      }
+      return;
+  }
+  if let Some(ay) = code.strip_prefix("RS:"){
+      if let Some((axis, v)) = ay.split_once(":"){
+          if let Ok(mut v) = v.parse::<i32>(){
+              v = v.clamp(-32768, 32767);
+              match axis {
+                  "X" => { let _ = self.device.position(&Position::RX, v);},
+                  "Y" => { let _ = self.device.position(&Position::RY, v);},
+                  _ => {}
+              }
+              let _ = self.device.synchronize();
+          }
+      }
+      return;
   }
 }
 
