9e82b4e3a5d6808839a25b7f992ca375fcd78afa
Youssif Al-Halawche <yhalawche@gmail.com>
2026-03-14 03:52:36 -0600

    welcome message and info

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,21 @@ use input::JoyDevice;
 mod server;
 mod input;
 mod debug_log;
+
+const VERSION : &str = "1.1";
+const WELCOME : &str = "
+    PLAYPORT ${VERSION} \n 
+    YOU WILL NEED THE ANDROID JOYSTICK CLIENT 
+    \n 
+    https://github.com/tygrisIQ/PlayPort_Client
+";
 fn main(){
     let port= std::env::args().nth(1).unwrap_or(String::from("8007"));
     debug_log!("port :{}", port);
     //device init
     let mut jdevice = input::JoyDevice::new();
     //network
+    println!("{}",WELCOME);
     thread::spawn(||{server::broadcast()});
     let (tx, rx) = mpsc::channel();
     let port_clone = port.clone();
